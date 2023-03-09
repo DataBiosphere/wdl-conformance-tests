1308d1da4ae649cb48f54aeaca419bb7f77b674c
Adam Novak <anovak@soe.ucsc.edu>
2023-03-09 16:50:43 -0500

    Add support for actually selecting versions

--- run.py
+++ run.py
@@ -115,6 +115,17 @@ def verify_outputs(expected_outputs, results_file, quiet):
             raise NotImplementedError('This output type has not been implemented yet.')
     return {'status': 'SUCCEEDED', 'reason': None}
 
+def announce_test(test_number, total_tests, test):
+    description = test['description']
+    print(f'\n[{test_number + 1}/{total_tests}] TEST {test_number}: {description}')
+
+def print_response(test_number, total_tests, response):
+    """
+    Log a test response that has a status and maybe a reason.
+    """
+    print(f'[{test_number + 1}/{total_tests}] TEST {test_number}: {response["status"]}!')
+    if response["reason"]:
+        print(f'    REASON: {response["reason"]}')
 
 def run_test(test_number, total_tests, test, runner, quiet):
     """
@@ -126,31 +137,56 @@ def run_test(test_number, total_tests, test, runner, quiet):
     wdl_file = os.path.abspath(test['wdl'])
     json_file = os.path.abspath(test['json'])
     args = test['args']
-    description = test['description']
     outputs = test['outputs']
     results_file = os.path.abspath(f'results-{uuid4()}.json')
     
     cmd = runner.format_command(wdl_file, json_file, results_file, args, quiet)
-    
-    print(f'\n[{test_number + 1}/{total_tests}] TEST {test_number}: {description}')
     print(f'    RUNNING: {cmd}')
     
-    def print_response(response):
-        """
-        Log a test success or failure.
-        """
-        print(f'[{test_number + 1}/{total_tests}] TEST {test_number}: {response["status"]}!')
-        if response["reason"]:
-            print(f'    REASON: {response["reason"]}')
-
     response = run_cmd(cmd=cmd, cwd=os.path.dirname(wdl_file), quiet=quiet)
     if response['status'] != 'SUCCEEDED':
-        print_response(response)
+        print_response(test_number, total_tests, response)
         return False
 
     response = verify_outputs(outputs, results_file, quiet=quiet)
-    print_response(response)
+    print_response(test_number, total_tests, response)
     return response['status'] == 'SUCCEEDED'
+    
+def check_test(test, versions_to_test):
+    """
+    Determine if a test should be skipped.
+    
+    Returns a response, with SUCCEEDED status if the test should be run, and
+    SKIPPED otherwise.
+    """
+    
+    # TODO: Tests have a versions_supported array for some reason but
+    # actually can only really be one version.
+    version_match = False
+    for version in test['versions_supported']:
+        if version in versions_to_test:
+            version_match = True
+            
+    if not version_match:
+        return {'status': 'SKIPPED', 'reason': f'Test only applies to versions: {",".join(test["versions_supported"])}'}
+        
+    return {'status': 'SUCCEEDED', 'reason': None}
+    
+def skip_test(test_number, total_tests, test, versions_to_test):
+    """
+    Return True if a test should be skipped.
+    
+    If a test is skipped, logs why.
+    """
+    
+    response = check_test(test, versions_to_test)
+    
+    if response['status'] == 'SKIPPED':
+        print_response(test_number, total_tests, response)
+        return True
+    
+    return False
+    
 
 def get_test_numbers(number_argument):
     ranges = [i for i in number_argument.split(',') if i]
@@ -168,38 +204,58 @@ def main(argv=sys.argv[1:]):
                         help='Suppress printing run messages.')
     parser.add_argument("--versions", "-v", default="1.0",
                         help='Select the WDL versions you wish to test against.')
-    parser.add_argument("--numbers", "-n", default='2',
+    parser.add_argument("--numbers", "-n", default=None,
                         help='Select the WDL test numbers you wish to run.')
     parser.add_argument("--runner", "-r", default='cromwell',
                         help='Select the WDL runner to use.')
     args = parser.parse_args(argv)
+    
+    # Get all the versions to test.
+    # Unlike with CWL, WDL requires a WDL file to declare a spacific version,
+    # and prohibits mixing file versions in a workflow, although some runners
+    # might allow it.
+    # But the tests all need to be for single WDL versions.
+    versions_to_test = set(args.versions.split(','))
 
     with open('conformance.json', 'r') as f:
         tests = json.load(f)
 
     total_tests = len(tests)
-
    
     if args.runner not in RUNNERS:
         print(f'Unsupported runner: {args.runner}')
         sys.exit(1)
     
     runner = RUNNERS[args.runner]
-    
+   
+    print(f'Testing runner {args.runner} on WDL versions: {",".join(versions_to_test)}\n') 
+   
     successes = 0
+    skips = 0
 
     test_numbers = get_test_numbers(args.numbers) if args.numbers else range(total_tests)
     for test_number in test_numbers:
+        test = tests[str(test_number)]
+        
+        announce_test(test_number, total_tests, test)
+        
+        if skip_test(test_number=test_number,
+                     total_tests=total_tests,
+                     test=test,
+                     versions_to_test=versions_to_test):
+            skips += 1
+            continue
+        
         success = run_test(test_number=test_number,
                            total_tests=total_tests,
-                           test=tests[str(test_number)],
+                           test=test,
                            runner=runner,
                            quiet=args.quiet)
         if success:
             successes += 1
     
-    print(f'{successes}/{total_tests} tests passed')
-    if successes < total_tests:
+    print(f'{total_tests - skips} tests run, {successes} succeeded, {total_tests - skips - successes} failed, {skips} skiped')
+    if successes < total_tests - skips:
         # Fail the program overall if tests failed.
         sys.exit(1)
 
