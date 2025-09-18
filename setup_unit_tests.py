d6c61bd2200682083a5565f491c3118180d38d8f
Adam Novak <anovak@soe.ucsc.edu>
2025-09-18 13:43:17 -0400

    Update example parsing regex

--- setup_unit_tests.py
+++ setup_unit_tests.py
@@ -26,9 +26,9 @@ from lib import convert_type
 
 import WDL
 
-# Use the same parsing regex as https://github.com/openwdl/wdl-tests/blob/58ff36209586ed69c9a64d3e0b151a343f12a4eb/scripts/extract_tests.py#L10-L13
+# Use the same parsing regex as https://github.com/openwdl/wdl-tests/blob/c9d59f6b7ef0f8e9f65cde92c9e70182c3afbd58/scripts/extract_tests.py#L10-L13
 TEST_RE = re.compile(
-    r"^<details>\s*<summary>\s*Example: (.+?)\s*```wdl(.+?)```\s*</summary>\s*(?:<p>\s*(?:Example input:\s*```json(.*?)```)?\s*(?:Example output:\s*```json(.*?)```)?\s*(?:Test config:\s*```json(.*)```)?\s*</p>\s*)?</details>$",
+    r"^<details>\s*<summary>\s*Example: (.+?)\s*```wdl(.+?)```\s*</summary>\s*(?:<p>\s*(?:Example input:\s*```json(.*?)```)?\s*(?:Example output:\s*```json(.*?)```)?\s*(?:Test config:\s*```json(.*?)```)?\s*</p>\s*)?</details>$",
     re.I | re.S,
 )
 FILENAME_RE = re.compile(r"(.+?)(_fail)?(_task)?.wdl")
