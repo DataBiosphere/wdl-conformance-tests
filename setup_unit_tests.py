58a41527760a3a6d8595857550135112addb29ab
Adam Novak <anovak@soe.ucsc.edu>
2025-10-23 14:44:36 -0400

    Handle nonexistent groups

--- setup_unit_tests.py
+++ setup_unit_tests.py
@@ -309,7 +309,7 @@ def generate_config_file(m: re.Match, output_dir: Path, version: str, all_data_f
 
     is_fail = is_fail or config_entry.get("fail", False)  # failure can also be specified in the json
 
-    config_entry["id"] = "".join(f.groups())
+    config_entry["id"] = "".join([part for part in f.groups() if part is not None])
 
     target_data: Optional[Dict[str, Any]] = None
     for m in extra_patch_data:
